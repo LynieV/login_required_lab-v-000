66fde23a403db8634b4eae5a2f97025107fc8723
Lynie McClimon <vmcclimon33@yahoo.com>
2020-04-02 01:28:08 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
-
+    
   end
 
   def destroy
     sessions.destroy :name
+  end
+
+  private
+
+  def current_user
+
   end
 
 end
